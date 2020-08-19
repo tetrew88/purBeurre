ed1567a13e6b8731a085ac4ba5a2efa840fcc036
tetrew88 <donovanmaurice@laposte.net>
2020-08-20 00:03:39 +0200

    launch to prod

--- purBeurre_project/settings.py
+++ purBeurre_project/settings.py
@@ -31,7 +31,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['purbeurreoc1.herokuapp.com/']
+ALLOWED_HOSTS = ['purbeurreoc1.herokuapp.com']
 
 
 # Application definition
